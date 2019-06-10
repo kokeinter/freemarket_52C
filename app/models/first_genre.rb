0c47c7bc6395e71b7a5241f423408d64bf3a98b4
sakodayujirou <greeeen20000801@ezweb.ne.jp>
2019-06-10 15:10:20 +0900

    モデル作成

---
+++ app/models/first_genre.rb
@@ -0,0 +1,6 @@
+class FirstGenre < ApplicationRecord
+  has_many :second_genres
+  has_many :items
+
+
+end
---
+++ db/migrate/20190610055350_create_first_genres.rb
@@ -0,0 +1,9 @@
+class CreateFirstGenres < ActiveRecord::Migration[5.2]
+  def change
+    create_table :first_genres do |t|
+      t.string  :genre  ,null: false
+
+      t.timestamps
+    end
+  end
+end
