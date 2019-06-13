f9214e4899c11c0d5ea21f58b7ee3cad69122a25
sakodayujirou <greeeen20000801@ezweb.ne.jp>
2019-06-13 13:12:00 +0900

    マイページのビュー

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,6 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  def mypage
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :users
   resources :items
   get "buy" => "items#buy"
+  get "users/:id/mypage" => "users#mypage"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
