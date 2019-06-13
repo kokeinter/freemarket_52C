f1585053fb87714c8a701d9a5a8692de70ffb968
sakodayujirou <greeeen20000801@ezweb.ne.jp>
2019-06-13 15:40:26 +0900

    本人情報確認のビュー

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_06_12_105757) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -107,13 +106,6 @@ ActiveRecord::Schema.define(version: 2019_06_12_105757) do
     t.integer "item_id", null: false
     t.integer "saler_id", null: false
     t.integer "buyer_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "provider"
-    t.string "uid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
