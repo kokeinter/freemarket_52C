4efdff0d9f3251bf3cd6e06c03f799353cdf61ea
こけ <koni.ken.engineer@gmail.com>
2019-06-13 15:52:55 +0900

    markup items#new

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
   resources :addresses ,only:[:new,:create]
   resources :creditcards ,only:[:new,:create]
   get 'complete'   =>    'users#complete'
+  
+  get '/buy' => 'items#buy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
