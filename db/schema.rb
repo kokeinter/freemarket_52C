409721ebe790b92a5814486b48b1e274ce18c64a
Azure0701 <s.tzw701@gmail.com>
2019-06-13 20:17:57 +0900

    update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2019_06_13_103222) do
-
-
-
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "post_id", null: false
@@ -145,6 +141,7 @@ ActiveRecord::Schema.define(version: 2019_06_13_103222) do
     t.string "birthday_year", null: false
     t.string "birthday_month", null: false
     t.string "birthday_day", null: false
+    t.string "tel"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -152,7 +149,6 @@ ActiveRecord::Schema.define(version: 2019_06_13_103222) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "tel"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
