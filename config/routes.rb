9f3104941985ebf3feef1040dd5a4c5444bf7040
こけ <koni.ken.engineer@gmail.com>
2019-06-18 19:14:20 +0900

    add routing"categories"

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,7 @@ Rails.application.routes.draw do
   
   resources :addresses ,only:[:new,:create]
   resources :creditcards ,only:[:new,:create]
+  resources :categories
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
