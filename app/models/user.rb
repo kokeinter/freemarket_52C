f822e634717eef7879c26cb560d4ad0addaf31b8
takaakikinki1128 <takaakikinki1128@icloud.com>
2019-06-21 12:36:21 +0900

    login-email7

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable  
-  validates :birthday_year,length: {is: 4 }
-  validates :birthday_month,length: {in:1..2 }
-  validates :birthday_day,length: {in:1..2 }
-
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
         has_many :comments, dependent: :destroy		
