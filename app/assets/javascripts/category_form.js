d783372c4f9269b32e7c12964361fd40a8d7a5a9
takaakikinki1128 <takaakikinki1128@icloud.com>
2019-06-23 19:12:52 +0900

    aaaa

---
+++ app/assets/javascripts/category_form.js
@@ -0,0 +1,20 @@
+$(function(){
+  function append_first_category(){
+    var html = `
+                <div class ="category-contents">
+                  <div class ="category-item">レディース</div><div class ="category-item">メンズ</div><div class ="category-item">ベビー・キッズ</div>
+                  <div class ="category-item">インテリア・住まい・小物</div><div class ="category-item">本・音楽・ゲーム</div>
+                  <div class ="category-item">おもちゃ・ホビー・グッズ</div><div class ="category-item">コスメ・香水・美容</div>
+                  <div class ="category-item">家電・スマホ・カメラ</div><div class ="category-item">スポーツ・レジャー</div><div class ="category-item">ハンドメイド</div>
+                  <div class ="category-item">チケット</div><div class ="category-item">自動車・オートバイ</div><div class ="category-item">その他</div>
+                  <div class ="category-item">カテゴリ一覧</div>
+                </div>`
+    return html
+  }
+  $(".categories").hover(function(){
+   var html = append_first_category();
+   $('.categories-btn') .append(html)
+   $('.categories').parent().css("display","block","position","fixed")
+   $('.default-container').parent().css("position","absolute")
+  });
+});
