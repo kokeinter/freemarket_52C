2949aac8bc90f0d971c84205f27791fd4593c87e
こけ <koni.ken.engineer@gmail.com>
2019-06-27 12:59:07 +0900

    modify settings for S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,9 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-if Rails.env.production?
-  CarrierWave.configure do |config|
+
+CarrierWave.configure do |config|
+  if Rails.env.production?
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
@@ -12,7 +13,10 @@ if Rails.env.production?
       aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
       region: 'ap-northeast-1'
     }
-
     config.fog_directory  = 'freemarket52c'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket52c'
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
   end
+end
