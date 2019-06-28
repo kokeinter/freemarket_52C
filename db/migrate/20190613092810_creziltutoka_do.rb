6d994bc4db0c1cfe78af7a6806bd5941231d6e21
sakodayujirou <greeeen20000801@ezweb.ne.jp>
2019-06-28 18:19:43 +0900

    Delete 20190613092810_creziltutoka_do.rb

--- db/migrate/20190613092810_creziltutoka_do.rb
+++
@@ -1,9 +0,0 @@
-class CreziltutokaDo < ActiveRecord::Migration[5.2]
-  def change
-    change_column :creditcards, :card_number, :string,unique: true,null: false
-    change_column :creditcards, :security_code, :string,null: false
-    change_column :creditcards, :limit_year, :string,null: false
-    change_column :creditcards, :limit_month, :string,null: false
-
-  end
-end
