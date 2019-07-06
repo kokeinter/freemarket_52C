063c7051d631c1acc95fc08cb9549209c48ec30a
sakodayujirou <greeeen20000801@ezweb.ne.jp>
2019-07-06 19:26:08 +0900

    コメントアウト

--- app/controllers/creditcards_controller.rb
+++ app/controllers/creditcards_controller.rb
@@ -1,29 +1,29 @@
-require "creditcard.rb"
-class CreditcardsController < ApplicationController
-  protect_from_forgery
+# require "creditcard.rb"
+# class CreditcardsController < ApplicationController
+#   protect_from_forgery
   
-  def new
-    @user=User.find(current_user.id)
-    if Rails.env.development?
-      @publickey= Rails.application.credentials.payjp[:public_test_key]
-    else
-      @publickey= Rails.application.credentials.payjp[:public_key]
-    end
-  end
+#   def new
+#     @user=User.find(current_user.id)
+#     if Rails.env.development?
+#       @publickey= Rails.application.credentials.payjp[:public_test_key]
+#     else
+#       @publickey= Rails.application.credentials.payjp[:public_key]
+#     end
+#   end
 
-  def create
-    if params[:payjpToken].present?
-      customer=Payjp::Customer.create(card: params[:payjpToken])
-      Creditcard.create(token: params[:payjpToken],user_id: creditcard_params[:user_id],customer_id: customer[:id])
-      redirect_to "/users/complete"
-    else
-      redirect_to new_creditcard_path
-    end
-  end
+#   def create
+#     if params[:payjpToken].present?
+#       customer=Payjp::Customer.create(card: params[:payjpToken])
+#       Creditcard.create(token: params[:payjpToken],user_id: creditcard_params[:user_id],customer_id: customer[:id])
+#       redirect_to "/users/complete"
+#     else
+#       redirect_to new_creditcard_path
+#     end
+#   end
 
-  private
+#   private
 
-  def creditcard_params
-    params.permit(:payjpToken,:user_id)
-  end
-end
+#   def creditcard_params
+#     params.permit(:payjpToken,:user_id)
+#   end
+# end
