a5ba19fb06534b6f9f0be1b6ff7c2ac440d91d85
こけ <koni.ken.engineer@gmail.com>
2019-07-06 18:49:52 +0900

    modify

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,8 +12,8 @@ CarrierWave.configure do |config|
       provider: 'AWS',
       # aws_access_key_id: Rails.application.secrets.aws_access_key_id,
       # aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
-      AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
-      AWS_SECRET_ACCESS_KEY:ENV["AWS_SECRET_ACCESS_KEY"],
+      # aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+      # aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
       region: 'ap-northeast-1'
     }
     config.fog_directory  = 'freemarket52c'
