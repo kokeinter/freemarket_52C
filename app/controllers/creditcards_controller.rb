4b8ab2964b5b41e1330ccd08531de4e4f1ac42f8
sakodayujirou <greeeen20000801@ezweb.ne.jp>
2019-07-08 18:26:01 +0900

    credential

--- app/controllers/creditcards_controller.rb
+++ app/controllers/creditcards_controller.rb
@@ -5,9 +5,9 @@ class CreditcardsController < ApplicationController
   def new
     @user=User.find(current_user.id)
     if Rails.env.development?
-      @publickey= Rails.application.credentials.payjp[:public_test_key]
+      @publickey= ENV["PUBLIC_TEST_KEY"]
     else
-      @publickey= Rails.application.credentials.payjp[:public_key]
+      @publickey= ENV["PUBLIC_KEY"]
     end
   end
 
