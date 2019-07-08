04947c6b67b34ff246a532fb92a966c23482056b
こけ <koni.ken.engineer@gmail.com>
2019-07-08 17:38:36 +0900

    modify

--- app/models/creditcard.rb
+++ app/models/creditcard.rb
@@ -1,9 +1,9 @@
 class Creditcard < ApplicationRecord
 
     if Rails.env.development?
-      Payjp::api_key = Rails.application.credentials.payjp[:secret_test_key]
+      Payjp::api_key = Rails.application.secrets.payjp[:secret_test_key]
     else
-      Payjp::api_key = Rails.application.credentials.payjp[:secret_key]
+      Payjp::api_key = Rails.application.secrets.payjp[:secret_key]
     end
 
     def self.pay(customer, amount)
