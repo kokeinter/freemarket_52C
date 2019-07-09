5869fc0e728893cc265196e5c07d61158b10e9e0
sakodayujirou <greeeen20000801@ezweb.ne.jp>
2019-07-09 19:34:36 +0900

    Update creditcards_controller.rb

--- app/controllers/creditcards_controller.rb
+++ app/controllers/creditcards_controller.rb
@@ -9,8 +9,8 @@ class CreditcardsController < ApplicationController
 
   def create
     if params[:payjpToken].present?
-      customer=Payjp::Customer.create(card: params[:payjpToken])
-      Creditcard.create(token: params[:payjpToken],user_id: creditcard_params[:user_id],customer_id: customer[:id])
+      # customer=Payjp::Customer.create(card: params[:payjpToken])
+      # Creditcard.create(token: params[:payjpToken],user_id: creditcard_params[:user_id],customer_id: customer[:id])
       redirect_to "/users/complete"
     else
       redirect_to new_creditcard_path
