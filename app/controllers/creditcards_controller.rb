be88713bde442517dc8d015214e002ac4fe1f6d8
sakodayujirou <greeeen20000801@ezweb.ne.jp>
2019-07-09 19:59:44 +0900

    Update creditcards_controller.rb

--- app/controllers/creditcards_controller.rb
+++ app/controllers/creditcards_controller.rb
@@ -9,8 +9,9 @@ class CreditcardsController < ApplicationController
 
   def create
     if params[:payjpToken].present?
+      Payjp::api_key = ENV["SECRET_TEST_KEY"]
       customer=Payjp::Customer.create(card: params[:payjpToken])
-      # Creditcard.create(token: params[:payjpToken],user_id: creditcard_params[:user_id],customer_id: customer[:id])
+      Creditcard.create(token: params[:payjpToken],user_id: creditcard_params[:user_id],customer_id: customer[:id])
       redirect_to "/users/complete"
     else
       redirect_to new_creditcard_path
