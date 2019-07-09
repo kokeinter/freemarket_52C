92889956a0bf95836ed8ce5f0963da85ea69b633
こけ <koni.ken.engineer@gmail.com>
2019-07-09 18:07:54 +0900

    modify

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
 app_path = File.expand_path('../../', __FILE__)
-ENV['BUNDLE_GEMFILE'] = app_path + "/current/Gemfile"
+ENV["BUNDLE_GEMFILE"] = File.join(File.expand_path("../../", __FILE__), "current", "Gemfile")
 
 worker_processes 1
 
