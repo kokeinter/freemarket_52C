887ae274715372825243e935910a437cdd575200
こけ <koni.ken.engineer@gmail.com>
2019-07-12 19:07:24 +0900

    S3

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,20 +1,20 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-  # process resize_to_fit: [800, 800]
+  include CarrierWave::MiniMagick
+  process resize_to_fit: [800, 800]
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  storage :fog
+  # storage :fog
 
 
 
-  # if Rails.env.production?
-  #   storage :fog
-  # else
-  #   storage :file
-  # end
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
 
 
 
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,23 +3,8 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 
-# CarrierWave.configure do |config|
-#   config.storage = :fog
-#   config.fog_provider = 'fog/aws'
-#   config.fog_credentials = {
-#     provider: 'AWS',
-#     aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-#     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-#     region: 'ap-northeast-1'
-#   }
-
-#   config.fog_directory  = 'freemarket52c2'
-#   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket52c2'
-# end
-
-
 CarrierWave.configure do |config|
-  # if Rails.env.production?
+  if Rails.env.production?
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
@@ -30,9 +15,9 @@ CarrierWave.configure do |config|
     }
     config.fog_directory  = 'freemarket52c2'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/freemarket52c2'
-  # else
-  #   config.storage = :file
-  #   config.enable_processing = false if Rails.env.test?
-  # end
+  else
+    config.storage = :file
+    config.enable_processing = false if Rails.env.test?
+  end
 end
 
